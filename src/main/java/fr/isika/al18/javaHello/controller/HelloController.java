b7b7ea0a404310ab021c44f3ebcdc51f3eb73a50
MyriamUmiko <myriam.sorhaindo@gmail.com>
2023-01-12 11:34:09 +0100

    ajout méthode get et post

--- src/main/java/fr/isika/al18/javaHello/controller/HelloController.java
+++ src/main/java/fr/isika/al18/javaHello/controller/HelloController.java
@@ -1,5 +1,22 @@
 package fr.isika.al18.javaHello.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Controller
 public class HelloController {
 
+	@GetMapping({"/","/index"})
+	public String index() {
+		return "index.html";
+	}
+	
+	@PostMapping("/hello")
+	public String index(@RequestBody String name, Model model) {
+		model.addAttribute("name", name);
+		return "hello.html";		
+	}
 }

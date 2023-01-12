19ae606b65a015ede3321ad8e9e4740da048659e
mrpopopolo <popopolo@gmail.com>
2023-01-12 17:22:17 +0100

    vérif input vide

--- src/main/java/fr/isika/al18/javaHello/controller/HelloController.java
+++ src/main/java/fr/isika/al18/javaHello/controller/HelloController.java
@@ -1,7 +1,10 @@
 package fr.isika.al18.javaHello.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +18,15 @@ public class HelloController {
 	}
 	
 	@PostMapping("/hello")
-	public String index(@RequestBody String name, Model model) {
-		String [] myTab = name.split("=");
-		model.addAttribute("name", myTab[1]);
+	public String index(HttpServletRequest req, ModelMap modelMap) {
+		String name = req.getParameter("name") != null 
+				&& !req.getParameter("name").isEmpty()
+				? req.getParameter("name")
+						:"Mr no name";
+				modelMap.put("name", name);
+	//	model.addAttribute("name", myTab[1]);
 		//System.out.println(name);
 		return "hello.html";		
 	}
+
 }
